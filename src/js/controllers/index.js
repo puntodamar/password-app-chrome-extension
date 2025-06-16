b3d067e0c8e52d6b9c34ea74e7e2957821346094
puntodamar <punto@privyid.tech>
2025-06-16 17:50:22 +0700

    Add: toggle password

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -3,11 +3,12 @@ import AppController from "./app_controller.js";
 import AuthController from "./auth_controller.js";
 import EntriesController from "./entries_controller.js";
 import SearchController from "./search_controller.js";
-
 import ClipboardController from "./clipboard_controller.js";
+import TogglePasswordController from "./toggle_password_controller.js";
 
 application.register("app", AppController);
 application.register("auth", AuthController);
 application.register("entries", EntriesController);
 application.register("search", SearchController);
 application.register("clipboard", ClipboardController);
+application.register("toggle-password", TogglePasswordController);
